6eed44acab16505867f79c31b37c968ab0969598
pandecamillo <160549468+pandecamillo@users.noreply.github.com>
2024-06-23 15:07:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let active = false;
 function menu() {
-    active = !active;
-    document.getElementById("menu").src = active ? "icon/close.png" : "icon/menu.png"
-    document.getElementById("navbar").style.display = active ? "block":"none"
+  active = !active;
+  document.getElementById("menu").src = active
+    ? "icon/close.png"
+    : "icon/menu.png";
+  document.getElementById("navbar").style.display = active ? "block" : "none";
 }
 
+function goToAdherer() {
+  location.href = "adherer.html";
+}
